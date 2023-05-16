41c0a02aaa59511660ca88fa92ebf4e153605343
Vinod Kumar Eraganaboina <Vinod-E>
2023-05-16 22:18:46 +0530

    Data security changes / Upload few test cases included

---
+++ cypress/e2e/TestScripts/UploadScoreScripts/EventTabViewAssessment.js
@@ -0,0 +1,65 @@
+import { MainTab } from "../../pageObjects/MenuTabs/MainTabsPage"
+import { AdvanceSearch } from "../../pageObjects/CommonPages/AdvanceSearchPage"
+import { EventgetByidActions } from "../../pageObjects/AllPages/EventGetByIdAction"
+import { AssessmentgetByidActions } from "../../pageObjects/AllPages/AssessmentGetByIdAction"
+
+
+
+//call pageObject methods
+const MenuTabs = new MainTab()
+const search = new AdvanceSearch()
+const event = new EventgetByidActions()
+const test = new AssessmentgetByidActions()
+
+
+// before Test Run
+before(() => {
+    cy.GobalVariables();
+    cy.saveLocalStorage();
+});
+
+beforeEach(() => {
+    cy.restoreLocalStorage();
+});
+
+
+export class EventTab{
+
+    event_tab(){
+        var event_name = localStorage.getItem("uploadscores_event")
+
+        MenuTabs.eventTab()
+        cy.wait(3000)
+        search.Filter()
+        search.event_name(event_name)
+        search.button_search()
+    }
+
+    event_assessment_tab(){
+        event.eventgetbyname()
+        event.event_actions()
+        event.assessment_action()
+    }
+
+    assessment_tab(){
+        test.assessmentgetbyname()
+        test.assessment_actions()
+        test.upload_score_action()
+    }
+
+    upload_assessment_emptyscores(){
+        var no_scores = localStorage.getItem("empty_scores_file")
+
+        test.upload_score_file(no_scores)
+        test.upload_file_button()
+        test.remove_score_count('Removed 10')
+    }
+
+    upload_assessment_scores(){
+        var scores = localStorage.getItem("scores_file")
+
+        test.upload_score_file(scores)
+        test.upload_file_button()
+        test.upload_score_count('Added 10')
+    }
+}
--- cypress/e2e/TestSuite/UploadScores.cy.js
+++ cypress/e2e/TestSuite/UploadScores.cy.js
@@ -1,7 +1,9 @@
 import { CRPOLogin } from "../TestScripts/UploadScoreScripts/CrpoLogin"
+import { EventTab } from "../TestScripts/UploadScoreScripts/EventTabViewAssessment"
 
 //Create an instance for import classes
 const crpo_login = new CRPOLogin()
+const eventtab = new EventTab()
 
 
 
@@ -11,14 +13,34 @@ describe('CRPO Login', function () {
         crpo_login.loginEnvironment()
     })
 
-    it('Logged Out from CRPO', ()=> {
-        crpo_login.logoutCRPO()
-    })
 })
 
 describe('View Event Assessment', function(){
 
-    it('', ()=> {
+    it('Event Tab Search', ()=> {
+        eventtab.event_tab()
+    })
 
+    it('Event Assessment', ()=> {
+        eventtab.event_assessment_tab()
+    })
+})
+
+describe('Assessment Upload Scores', function(){
+
+    it('Assessment Get By Name', ()=> {
+        eventtab.assessment_tab()
+    })
+
+    it('upload Empty scores', ()=> {
+        eventtab.upload_assessment_emptyscores()
+    })
+
+    it('upload scores', ()=> {
+        eventtab.upload_assessment_scores()
+    })
+
+    it.skip('Logged Out from CRPO', ()=> {
+        crpo_login.logoutCRPO()
     })
 })
